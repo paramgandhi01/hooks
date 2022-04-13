d502e6d9d470cb761b00e0974d8376be0738910d
paramgandhi01 <92042054+paramgandhi01@users.noreply.github.com>
2022-04-13 13:17:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import USESTATE from "./components/USESTATE"
+import Useref from "./components/Useref"
+import Usememo from "./components/Usememo"
+import Usecallback from "./components/Usecallback"
+import './App.css';
+import { useCallback, useRef, useState } from "react";
+import Useeffect from "./components/Useeffect";
+import Usereducer from "./components/Usereducer";
+import Usereducer1 from "./components/Usereducer1";
+import Usereducer2 from "./components/Usereducer2";
+
+function App() {
+  return (
+    <>
+    {/* <USESTATE/>
+    <Useref/>
+    <Usememo/> */}
+    {/* <Usecallback/> */}
+    {/* <Useeffect/> */}
+    {/* <Usereducer/> */}
+   {/* <Usereducer1/>  */}
+    <Usereducer2/>
+    </>
+  );
+}
+
+export default App;
